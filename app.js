acddd9a74e85c40fa3fefbcff54b2094d1734d63
lucasBarbeito <lucas.barbeito@greencodesoftware.com>
2023-08-19 15:41:32 -0300

    Adds a  404 when passing a non-existing endpoint

--- app.js
+++ app.js
@@ -5,4 +5,16 @@ const usersRoutes = require("./api/routes/users");
 
 app.use("/users", usersRoutes);
 
+app.use((req, res, next) => {
+  res.status(404).json({
+    error: "Not Found",
+    message: "The requested resource was not found.",
+    availableRoutes: [
+      "/users",
+      "/users/:username/details",
+      "/users/:username/repos",
+    ],
+  });
+});
+
 module.exports = app;
